9e986af3a3ebd0ee48d74bff6bfb9b8c0136c684
Naga Nikhil Bijjala <naganikhil.bijjala@gmail.com>
2025-02-02 10:05:19 -0500

    feat: create a form for add user

--- src/pages/Dashboard.jsx
+++ src/pages/Dashboard.jsx
@@ -1,17 +1,34 @@
 import React from "react";
 import { fetchData } from "../helpers";
 import { useLoaderData } from "react-router-dom";
+
+// components
+import { Intro } from "../components/Intro";
+import { toast } from "react-toastify";
+
 // loader
 export function dashboardLoader() {
   const userName = fetchData("userName");
   return { userName };
 }
 
+// action
+export async function dashboardAction({ request }) {
+  const data = await request.formData();
+  const formData = Object.fromEntries(data);
+  try {
+    localStorage.setItem("userName", JSON.stringify(formData.userName));
+    return toast.success(`Welecome, ${formData.userName}`);
+  } catch (e) {
+    throw new Error("There was a problem creating your account.");
+  }
+}
+
 export const Dashboard = () => {
   const { userName } = useLoaderData();
   return (
-    <div>
-      <h1> {userName} </h1>
-    </div>
+    <>
+      <h1> {userName ? <p> {userName}</p> : <Intro />} </h1>
+    </>
   );
 };
